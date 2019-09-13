c5c9dda5528e88647ce83c7ebe34a84f1f2f8fe7
Aleksandr Kolesnyk <shepard.via.n@gmail.com>
2019-09-13 23:09:39 +0300

    Add files via upload

---
+++ 1.0_1-intro2.js
@@ -0,0 +1,10 @@
+<!-- add numbers from 1 to 10 together and print out the result 1 + 2 + ... +10 = 55-->
+
+let total =0, count =1;
+while (count <= 10) {
+    total += count;
+    count += 1;
+}
+console.log(total);
+
+// is it comment ?
