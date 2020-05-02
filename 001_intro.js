7af1b0d8e2a37691d919aae0d06fd59703176963
a-kolesnyk <shepard.via.n@gmail.com>
2020-05-02 17:06:20 +0200

    newUpdate

---
+++ 001_intro.js
@@ -0,0 +1,32 @@
+"use strict";
+alert('«Я JavaScript!».');
+
+let user = 'John'
+    , age = 25
+    , message = 'Hello2';
+
+alert(message);
+alert(age*2);
+
+const myBday = '20.12.1993'; 
+alert(myBday);
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+let color = COLOR_RED;
+alert(COLOR_RED);
+
+    //task001
+let admin
+    , name;
+
+name = 'John';
+admin = name;
+alert(admin);
+
+let myPrivatePlanet;
+let currentUserName;
+
+
