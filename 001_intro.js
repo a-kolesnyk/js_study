4447f4b42fc61487652522a3ec097c599ecde641
a-kolesnyk <shepard.via.n@gmail.com>
2020-05-05 00:03:22 +0200

    newMarkdownAgain

--- 001_intro.js
+++ 001_intro.js
@@ -28,12 +28,58 @@ admin = name;
 alert(admin);
 
 let myPrivatePlanet;
-let currentUserName; */
+let currentUserName; 
 
 let value = true;
 alert(typeof value); // boolean
 
 value = Number(value); // теперь value это строка "true"
 alert(typeof value); // number
+
+
+alert("" + 1 + 0 ); // 10
+alert("1 + 0"); // 1 + 0
+alert(1 + 0); // 1
+
+alert(true + false); //1
+*/
+
+
+console.log("answ1:", true + false);  // converts to 1 / 0 -> 1+0 = 1;
+console.log("answ2:", "" + 1 + 0); // "" + 1 -> "1"; "1"+ 0 = "10"
+console.log("answ3:", "2" + "3");
+console.log("answ4:", "" - 1 + 0); 
+console.log("answ5:", 4 + 5 + "px"); // concatenation 
+console.log("answ6:", "4px" - 2); // ошибка чтения числа 'px'
+
+let answ1
+    ,answ1updated
+    ,answ2
+    ,answ3
+    ,answ4
+    ,answ5
+    ,answ6
+    ,answ7;
+
+answ1 = true + false;
+answ1updated = String(answ1);
+answ7 = 0 + 0;
+
+console.log(typeof answ1);
+console.log(typeof answ1updated);
+console.log(answ1updated);
+console.log(answ1 + answ1updated);
+console.log(typeof `${0 + 0}`);
+console.log(9 % 2); 
+console.log(9 / 2);
+console.log(2 ** 2);
+
+let counter = 2
+    , counter2
+    , counter3 = 3;
+++counter;
+console.log(counter + counter3);
+
+
 
 
